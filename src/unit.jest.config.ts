27aeaee62142622272f3a7db1912b657660a13f3
Abhi Markan <abhi.markan@justice.gov.uk>
2026-01-14 11:38:53 +0000

    feat(10-sca): fixed jest configuration for ESM

--- src/unit.jest.config.ts
+++ src/unit.jest.config.ts
@@ -1,10 +1,12 @@
-module.exports = {
+import type { Config } from "@jest/types";
+
+export default {
   preset: "ts-jest",
   transform: {
     "^.+\\.ts$": "ts-jest",
   },
-  testMatch: ["<rootDir>/src/**/*.test.ts"],
+  testMatch: ["**/*.test.ts"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageReporters: ["text", "text-summary"],
   testTimeout: 20000, // 20 seconds
-};
+} as Config.InitialOptions;
